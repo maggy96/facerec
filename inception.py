f6f9c649a594807a25a017e17e71017d0a78aef7
Magnus Görlitz <magnus.goerlitz@googlemail.com>
2016-12-07 12:18:41 +0100

    Typos

--- inception.py
+++ inception.py
@@ -2,15 +2,10 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.applications.inception_v3 import InceptionV3
 from keras.models import Model 
 from keras.layers import Dense, GlobalAveragePooling2D
-from keras import backend as K
-
 
 def img_gen(path='data'):
     train_datagen = ImageDataGenerator(
         rescale=1./255)
-        # shear_range=0.2,
-        # zoom_range=0.2,
-        # horizontal_flip=True)
 
     train_generate = train_datagen.flow_from_directory(
         path,
@@ -45,4 +40,4 @@ if __name__ == "__main__":
         samples_per_epoch=5749,
         nb_epoch=50)
 
-    model.save('catdog.h5')
+    model.save('face.h5')
